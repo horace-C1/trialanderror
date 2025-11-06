5b83aef684f8894eb024e62b9df1ced925ad24d7
horace-C1 <horacechan9951@gmail.com>
2025-11-06 14:09:36 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,36 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('tracker-cache').then(cache => cache.addAll(['./', './index.html', './manifest.json']))
+// Change this version number whenever you update files
+const CACHE_VERSION = 'v2'; // Change from 'v1' to 'v2', etc.
+const CACHE_NAME = `my-app-${CACHE_VERSION}`;
+
+// Install event - cache your files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        // other assets
+      ]);
+    })
   );
+  // Force the waiting service worker to become the active service worker
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  // Take control of all pages immediately
+  return self.clients.claim();
 });
