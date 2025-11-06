3d59a6d1d54a82ff695752613cab1826a0157101
horace-C1 <horacechan9951@gmail.com>
2025-11-06 13:47:52 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,9 @@
-const CACHE_NAME = 'protocol-tracker-cache-v2'; // increment on update
-const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  // icons etc
-];
-
-// Install
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)).then(()=>self.skipWaiting()));
-});
-
-// Activate
-self.addEventListener('activate', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+    caches.open('tracker-cache').then(cache => cache.addAll(['./', './index.html', './manifest.json']))
   );
 });
 
-// Fetch
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(c => c||fetch(e.request)));
-});
-
-// Message
-self.addEventListener('message', e => {
-  if(e.data?.type==='SKIP_WAITING') self.skipWaiting();
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
