8794ac939d9dd4021f0a4f5733117b38f9e22d2a
horace-C1 <horacechan9951@gmail.com>
2025-11-06 13:19:03 -0600

    Refactor service worker for cache management
    Updated service worker to use a new cache name and added activate event handling.

--- sw.js
+++ sw.js
@@ -1,9 +1,20 @@
+const cacheName = 'protocol-tracker-cache-v2'; // increment each update
+
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('tracker-cache').then(cache => cache.addAll(['./', './index.html', './manifest.json']))
+    caches.open(cacheName).then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/manifest.json',
+      // add any icons or CSS
+    ])).then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
